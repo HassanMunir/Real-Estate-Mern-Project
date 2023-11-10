6ac2da06d53111436d68d33fb07ef67e17a3d510
Hassan Munir <91518406+HassanMunir@users.noreply.github.com>
2023-11-10 12:11:51 +0500

    error-utils

---
+++ api/utils/error.js
@@ -0,0 +1,7 @@
+// Conditional Force Error
+export const errorHandler = (statusCode, message) => {
+  const error = new Error();
+  error.statusCode = statusCode;
+  error.message = message;
+  return error;
+};
